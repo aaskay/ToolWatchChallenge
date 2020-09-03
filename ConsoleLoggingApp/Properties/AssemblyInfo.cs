d5a83ec52328558444f12cd7d9460ccbed3ecc92
aaskay <aaskay@gmail.com>
2020-09-03 15:03:00 -0600

    Part of name change

--- ConsoleLoggingApp/Properties/AssemblyInfo.cs
+++ ConsoleLoggingApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleLoggingApp")]
+[assembly: AssemblyTitle("TestHarness")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleLoggingApp")]
+[assembly: AssemblyProduct("TestHarness")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
